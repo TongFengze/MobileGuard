dd053ffe4b7b95508b5a21d09869aecb5dbe8ef5
TongFengze <39987648+TongFengze@users.noreply.github.com>
2018-06-06 22:05:05 +0800

    Initial commit

---
+++ app/src/main/java/com/solomon/mobileguard/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.solomon.mobileguard.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.solomon.mobileguard.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
